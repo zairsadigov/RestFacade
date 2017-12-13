7d87bdb13149a23ed8ef97a8a982e6b7dc0c557f
Zair Sadiqov <zairsadiqov@gmail.com>
2017-12-13 01:35:41 +0100

    initial spring boot project with web, jpa, embedded H2

---
+++ src/main/java/com/luxoft/ubs/restfacade/RestFacadeApplication.java
@@ -0,0 +1,12 @@
+package com.luxoft.ubs.restfacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFacadeApplication.class, args);
+	}
+}
