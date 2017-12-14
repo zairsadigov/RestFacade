a883bbac25648e9f013f58635cd92f6ea641b526
Zair Sadiqov <zairsadiqov@gmail.com>
2017-12-14 09:19:45 +0100

    adds MovieController and tests

---
+++ src/main/java/com/luxoft/ubs/restfacade/web/MovieController.java
@@ -0,0 +1,64 @@
+package com.luxoft.ubs.restfacade.web;
+
+import com.luxoft.ubs.restfacade.entity.Comment;
+import com.luxoft.ubs.restfacade.entity.Movie;
+import com.luxoft.ubs.restfacade.service.CommentService;
+import com.luxoft.ubs.restfacade.service.MovieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@EnableWebMvc
+public class MovieController {
+
+    private MovieService movieService;
+    private CommentService commentService;
+
+    public MovieController(MovieService movieService, CommentService commentService) {
+        Assert.notNull(movieService, "movieService is required");
+        Assert.notNull(commentService, "commentService is required");
+
+        this.movieService = movieService;
+        this.commentService = commentService;
+    }
+
+    @GetMapping(path = "/{id:\\d+}")
+    public ResponseEntity<MovieAndComments> getMovieAndCommentsByMovieId(@PathVariable Long id) throws ExecutionException, InterruptedException {
+        CompletableFuture<Movie> completableFuture = movieService.getMovieById(id);
+        CompletableFuture<Collection<Comment>> collectionCompletableFuture = commentService.getCommentsByMovieId(id);
+
+        return ok(new MovieAndComments(completableFuture.get(), collectionCompletableFuture.get()));
+    }
+
+    class MovieAndComments {
+        private Movie movie;
+        private Collection<Comment> commentCollection;
+
+        MovieAndComments(Movie movie, Collection<Comment> commentCollection) {
+            this.movie = movie;
+            this.commentCollection = commentCollection;
+        }
+
+        public Movie getMovie() {
+            return movie;
+        }
+
+        public Collection<Comment> getCommentCollection() {
+            return commentCollection;
+        }
+
+        boolean addComments(Collection<Comment> commentCollection) {
+            return this.commentCollection.addAll(commentCollection);
+        }
+    }
+}
---
+++ src/test/java/com/luxoft/ubs/restfacade/web/MovieControllerTest.java
@@ -0,0 +1,91 @@
+package com.luxoft.ubs.restfacade.web;
+
+import com.luxoft.ubs.restfacade.entity.Comment;
+import com.luxoft.ubs.restfacade.entity.Movie;
+import com.luxoft.ubs.restfacade.service.CommentService;
+import com.luxoft.ubs.restfacade.service.MovieService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class MovieControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MovieService movieService;
+    @Autowired
+    private CommentService commentService;
+
+    private Movie movie;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+
+        this.movie = new Movie("Test Movie Title", "Test movie description");
+    }
+
+    @Test
+    @Rollback
+    public void getMovieAndCommentsByMovieId() throws Exception {
+        CompletableFuture<Movie> completableFuture = movieService.addMovie(movie);
+        Movie retrievedMovie = completableFuture.get();
+
+        Comment comment1 = new Comment("user1", "This is 1st scene to watch", retrievedMovie.getId());
+        Comment comment2 = new Comment("user1", "This is 2nd scene to watch", retrievedMovie.getId());
+
+        CompletableFuture<Comment> commentCompletableFuture1 = commentService.addComment(comment1);
+        CompletableFuture<Comment> commentCompletableFuture2 = commentService.addComment(comment1);
+        Comment retrievedComment1 = commentCompletableFuture1.get();
+        Comment retrievedComment2 = commentCompletableFuture2.get();
+
+        mockMvc.perform(get("/" + retrievedMovie.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"movie\":{\"id\":" +
+                        retrievedMovie.getId() +
+                        ",\"title\":\"" +
+                        retrievedMovie.getTitle() +
+                        "\",\"description\":\"" +
+                        retrievedMovie.getDescription() +
+                        "\"},\"commentCollection\":[{\"id\":" +
+                        retrievedComment1.getId() +
+                        ",\"username\":\"" +
+                        retrievedComment1.getUsername() +
+                        "\",\"message\":\"" +
+                        retrievedComment1.getMessage() +
+                        "\",\"movieId\":" +
+                        retrievedComment1.getMovieId() +
+                        "},{\"id\":" +
+                        retrievedComment2.getId() +
+                        ",\"username\":\"" +
+                        retrievedComment2.getUsername() +
+                        "\",\"message\":\"" +
+                        retrievedComment2.getMessage() +
+                        "\",\"movieId\":" +
+                        retrievedComment2.getMovieId() +
+                        "}]}"));
+    }
+
+}
